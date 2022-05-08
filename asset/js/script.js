c087ee3c46f6aebaa0ed14b9c21811ee81762ab5
razaquat.ahmed <razaquat@hotmail.co.uk>
2022-05-08 13:35:57 +0100

    added_elements_index

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,3 +1,6 @@
+let questionIndex = 2
+
+
 const questions = [
     {
       question: "Who invented JavaScript?",
@@ -11,96 +14,120 @@ const questions = [
     },
     {
       question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm",
-        d: ""
-      },
-      correctAnswer: "c"
+      answers: [
+        "Node.js",
+        "TypeScript",
+        "npm",
+        "vpn"
+      ],
+      correctAnswer: 2
     },
     {
       question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
+      answers: [
+        "Angular",
+        "jQuery",
+        "RequireJS",
+        "ESLint"
+      ],
+      correctAnswer: 3
     }
   ];
-var Timervalue = 10
+
 
 const main = document.getElementById("main")
+const intro = document.getElementById("Question-section")
 const startButton = document.getElementById("startButton")
-
-
-var timeRemainingsection = document.createElement("div");
-timeRemainingsection.setAttribute("style", "display:flex;");
-
-var timeRemaining = document.createElement("div");
-timeRemaining.textContent = "Time Remaining : ";
-timeRemaining.setAttribute("style", "font-size: xx-large; text-align:left;");
-
-var timeRemainingvalue = document.createElement("div");
-timeRemainingvalue.setAttribute("style", "font-size: xx-large; text-align:left;");
-
-var Qandmain = document.createElement ("div")
-//Qandmain.textContent = qheading optionList
-//var qheading = document.createElement("h2")
-//var optionList = document.createElement("ul")
-
-
-
+var Timervalue = 10
 
 startButton.addEventListener("click",startGame)
 
 function startGame(){
     console.log("hello")
-    renderQuestionSection();
     startTimer();
+    renderQuestionSection();
+    
 }
+function startTimer() {
+  intro.remove();
+     
+  
+  
+  var timeInterval = setInterval(function () {
+    if (Timervalue > 0) {
+      console.log (Timervalue + ' seconds remaining');
+      Timervalue--;
+    }
+        else {
+    console.log("game Over") 
+   clearInterval(timeInterval);
+  }
+
+  }, 1000);
+    }
 
 function renderQuestionSection() {
 
   console.log("renderquestion")
 
-  const main2 = document.createElement("main")
-  main2.setAttribute("class", "questionSec")
+  const currentQuestion = questions[questionIndex];
 
- const timsec = document.createElement("section")
- timsec.setAttribute("class","timerSection" )
+ const questSec = document.createElement("section");
+ questSec.setAttribute("class", "questionSec");
+
+ const timersec = document.createElement("section");
+ timersec.setAttribute("class","timerSection" );
+
+  
+ const timerTex = document.createElement("div");
+ timerTex.setAttribute("class", "timeRtext");
+ timerTex.textContent = "Time Remainig";
+
+ const timerVal = document.createElement("div");
+ timerVal.setAttribute("class", "timerbox");
+ 
+
+ const qbox = document.createElement("section");
+qbox.setAttribute("class","qbox" );
 
  
+const questionB = document.createElement("div");
+questionB.setAttribute("class","question" );
+questionB.textContent = `${questionIndex + 1}. ${currentQuestion.question}`;
+
+const ul = document.createElement("ul");
+ul.setAttribute("class", "options");
+
+
+const li1 = document.createElement("li");
+li1.setAttribute("data-value", currentQuestion.answers[0]);
+  li1.textContent = currentQuestion.answers[0];
+
+const li2 = document.createElement("li");
+li2.setAttribute("data-value", currentQuestion.answers[1]);
+  li2.textContent = currentQuestion.answers[1]
+
+const li3 = document.createElement("li");
+li3.setAttribute("data-value", currentQuestion.answers[2]);
+li3.textContent = currentQuestion.answers[2]
+
+const li4 = document.createElement("li");
+li4.setAttribute("data-value", currentQuestion.answers[3]);
+li4.textContent = currentQuestion.answers[3]
+
+ul.append(li1,li2,li3,li4);
+
+timersec.append(timerTex,timerVal);
+
+
+qbox.append(questionB,ul);
+
+main.append(timersec,qbox);
+console.log(Timervalue)
             
 }
 
 
-function startTimer() {
-    main.remove();
-    helloHeading.remove();
-    rules.remove();
-    startButton.remove();
-    QuestionSection.appendChild(timeRemaining);
-    QuestionSection.appendChild(timeRemainingvalue);
-    //qheading.appendChild("hello");
-    //QuestionSection.createElement("h1");
-    //QuestionSection.textContent("title for question")
 
-
-
-    var timeInterval = setInterval(function () {
-      if (Timervalue > 0) {
-        console.log (Timervalue + ' seconds remaining');
-        Timervalue--;
-        timeRemainingvalue.innerHTML= Timervalue ;}
-          else {
-      console.log("game Over") 
-     clearInterval(timeInterval);
-    }
-
-    }, 1000);
-      }
 
 
